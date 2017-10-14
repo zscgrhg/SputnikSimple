900693dc577d65914a4dcde6d3ac925409a253d7
zscgrhg <zscgrhg@qq.com>
2017-10-14 22:38:25 +0800

    Initial commit

---
+++ src/automation/groovy/sputnik/HomePageSpec.groovy
@@ -0,0 +1,21 @@
+package sputnik
+
+import geb.Browser
+import geb.spock.GebReportingSpec
+
+class HomePageSpec extends GebReportingSpec {
+    def "Trivial Geb test for homepage"() {
+        when: "go to homepage"
+        Browser.drive {
+
+            go "http://www.biying.cn"
+            report "login screen2"
+            go "http://www.baidu.com"
+            report "login screen1"
+        }
+        then: "First page should load"
+        report "login screen3"
+        report "login screen4"
+        title == "百度一下，你就知道"
+    }
+}
